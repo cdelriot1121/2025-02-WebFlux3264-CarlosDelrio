c95172d3a12e3e0f209b1c05b1e277daa3f435b3
cdelriot1121 <carandresdelrio1121@gmail.com>
2025-09-13 15:36:43 -0500

    feat: creacion de las entidades

---
+++ TallerJdbc/src/entities/EstadoCivil.java
@@ -0,0 +1,9 @@
+package entities;
+
+public enum EstadoCivil {
+    SOLTERO,
+    CASADO,
+    VIUDO,
+    UNION_LIBRE,
+    DIVORCIADO
+}
---
+++ TallerJdbc/src/entities/Estudiantes.java
@@ -0,0 +1,114 @@
+package entities;
+
+import java.util.Objects;
+
+
+public class Estudiantes {
+    
+   
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private Integer edad;
+
+    
+    private EstadoCivil estadoCivil;
+    
+    
+    public Estudiantes() {
+    }
+    
+    
+    public Estudiantes(String nombre, String apellido, String correo, Integer edad, EstadoCivil estadoCivil) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.edad = edad;
+        this.estadoCivil = estadoCivil;
+    }
+    
+    
+    public Estudiantes(Long id, String nombre, String apellido, String correo, Integer edad, EstadoCivil estadoCivil) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.edad = edad;
+        this.estadoCivil = estadoCivil;
+    }
+    
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getApellido() {
+        return apellido;
+    }
+    
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    public String getCorreo() {
+        return correo;
+    }
+    
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    public Integer getEdad() {
+        return edad;
+    }
+    
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+    
+    public EstadoCivil getEstadoCivil() {
+        return estadoCivil;
+    }
+    
+    public void setEstadoCivil(EstadoCivil estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+    
+    @Override
+    public String toString() {
+        return "Estudiantes{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", correo='" + correo + '\'' +
+                ", edad=" + edad +
+                ", estadoCivil=" + estadoCivil +
+                '}';
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Estudiantes that = (Estudiantes) o;
+        return Objects.equals(id, that.id);
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
